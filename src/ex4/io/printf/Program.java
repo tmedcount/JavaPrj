b9e976b9c163569ebb7aa51bb66024a52320487a
tmedcount <tmedcount@gmail.com>
2021-07-29 17:24:53 +0900

    2021-07-29

--- src/ex4/io/printf/Program.java
+++ src/ex4/io/printf/Program.java
@@ -11,18 +11,18 @@ public class Program {
 		kor3 = 80;
 
 		total = kor1 + kor2 + kor3;
-		avg = total / 3;
+		avg = total / 3.0f;
 		
 		//  --------성적 출력 부분--------------------------------
 		System.out.println("┌─────────────────────┐");
-		System.out.println("│       성적 출력               │");
+		System.out.println("│       성적 출력                │");
 		System.out.println("└─────────────────────┘");
 		
-		System.out.println("국어1: 0");
-		System.out.println("국어2: 0");
-		System.out.println("국어3: 0");
-		System.out.println("총점: 0");
-		System.out.println("평균: 0.00");
+		System.out.printf("\t국어1: %3d\n", kor1);
+		System.out.printf("\t국어2: %3d\n", kor2);
+		System.out.printf("\t국어3: %3d\n", kor3);
+		System.out.printf("\t총점: %3d\n", total);
+		System.out.printf("\t평균: %6.2f\n", avg);
 		System.out.println("───────────────────────");
 		
 		System.out.print(80);
